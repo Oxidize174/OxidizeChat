84d1396a31b533820c9ec846a855f1b8890d85a5
Oxydize <beldmitry1@gmail.com>
2022-01-04 20:21:55 +0500

    1)Запрос сообщений 2)Передали текст сообщения

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,37 @@
-$( document ).ready(function() {
-    $("#send_button").click(function (){
+$(document).ready(function () {
+    $.ajax({
+        url: "http://localhost:8080/api/messages/get",
+        method: "GET",
+        context: document.body,
+    }).done(function (data) {
+        console.log(data)
+        var messages = "";
+        $.each(data, function (index, val) {
+            messages += `
+                <div class="message">
+                    <div class="user">${val.userId}</div>
+                    <div class="text">${val.text}</div>
+                </div>
+           `
+        })
+        $("#messages").html(messages)
+    })
+
+    $("#send_button").click(function () {
         $.ajax({
             url: "http://localhost:8080/api/messages/create",
             method: "POST",
             context: document.body,
             data: {
-            "userId": 1,
-            "text": "Hello!"
+                "userId": 1,
+                "text": $("#input_message").val()
             }
-        }).done(function(data) {
+
+        }).done(function (data) {
             console.log(data)
         });
-    } )
+
+    })
 
 });
 
