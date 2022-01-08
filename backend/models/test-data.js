b5343c84d48d243d14e3da0776c5529b968b3be7
Oxydize <beldmitry1@gmail.com>
2022-01-08 18:59:51 +0500

    1)Доработан внешний стиль 2)Реструктуризация папки frontend 3)Новый файл index.css

--- backend/models/test-data.js
+++ backend/models/test-data.js
@@ -1,20 +1,6 @@
 module.exports = (db) => {
     db.user.bulkCreate([
-        {
-            "name": "Dmitry",
-            "login": "Oxydize",
-            "password": "123456",
-        },
-        {
-            "name": "Ilya",
-            "login": "Iksent",
-            "password": "123456",
-        },
-        {
-            "name": "Anton",
-            "login": "New",
-            "password": "123456",
-        }
+
     ]).then(() => {
         console.log('TEST DATA > Users inserted')
     }).catch(err => {
--- frontend/js/chat.js
+++ frontend/js/chat.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
         $.ajax({
             url: API_BASE + "/user/logout",
             method: "POST"
-        }).done(function (data) {
+        }).done(function () {
             window.location.href = 'login.html'
         })
     })
